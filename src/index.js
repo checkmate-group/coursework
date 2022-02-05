d02c50f322d5b99cfa374a5f936029705b8fb3fe
Nero-DevOps <headt@roehampton.ac.uk>
2022-02-05 02:09:32 +0000

    Removed Only Example To Show Dockerfile Working
    This is no longer needed as was only used to show that our first docker file worked and was setup correct

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')();
-
-app.get('/', (req, res ) => 
-    res.json({ message: 'This is a big test' }) 
-);
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
