b733b6b040f6199befd2133d03c32e37dcc71047
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2022-02-25 05:49:40 +0000

    Many changes to the core of the project
    Mainly re-doing the core of the project in terms of design and how the code is structure. There is still lots of work to do however, I think bring able to view all reports from one place is the correct way to go.

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express   = require("express");
+const routes    = require("./src/routes/app");
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app   = express();
+const port  = process.env.PORT || 3000;
 
-app.use(express.static('src/public'));
+app.use(express.static("src/public"));
 
-app.set('view engine','pug');
-app.set('views','./src/views');
+app.set("view engine", "pug");
+app.set("views", "./src/views");
 
-const routes = require('./src/routes/app');
+app.use("/", routes);
 
-app.use('/',routes);
+app.use((req, res, next) => {
+    res.status(404).render("404");
+});
 
-app.listen(port,()=>{
-    console.log("server runing at http://localhost:"+port);
+app.listen(port, () => {
+    console.log("server runing at http://localhost:" + port);
 })
---
+++ src/public/js/view_table.js
@@ -0,0 +1,75 @@
+const viewTableDropdown = document.getElementById("view-select");
+const viewTableButton   = document.getElementById("view-table-button");
+const viewTableHeader   = document.getElementById("view-header");
+const table             = document.getElementById("table");
+
+// view report button
+viewTableButton.addEventListener("click", e => {
+    // toggle table from hidden to visible
+    // table.classList = "d-block";
+
+    // set the table header name baased on the currently selected option from the menu
+    // viewTableHeader.innerText = viewTableDropdown.options[viewTableDropdown.selectedIndex].text;
+
+    // redirect to specific sub-view that is handled by express.js
+    switch(viewTableDropdown.selectedIndex) {
+        case 0:
+            window.location.href = "/viewer/world_population/";
+            break;
+        case 1:
+            window.location.href = "/viewer/world_countries_by_population/";
+            break;
+        case 2:
+            window.location.href = "/viewer/world_countries_by_population_in_region/";
+            break;
+        case 3:
+            window.location.href = "/viewer/world_countries_by_population_in_continent/";
+            break;  
+        case 4:
+            window.location.href = "/viewer/world_cities_by_population/";
+            break;
+        case 5:
+            window.location.href = "/viewer/world_cities_by_population_in_region/";
+            break;
+        case 6:
+            window.location.href = "/viewer/world_cities_by_population_in_district/";
+            break;
+        case 7:
+            window.location.href = "/viewer/world_cities_by_population_in_country/";
+            break;
+        case 8:
+            window.location.href = "/viewer/world_cities_by_population_in_continent/";
+            break;
+        case 9:
+            window.location.href = "/viewer/world_capital_cities_by_population/";
+            break;
+        case 10:
+            window.location.href = "/viewer/world_capital_cities_by_population_in_region/";
+            break;
+        case 11:
+            window.location.href = "/viewer/world_capital_cities_by_population_in_continent/";
+            break;
+        case 12:
+            window.location.href = "/viewer/region_population/";
+            break;
+        case 13:
+            window.location.href = "/viewer/population_languages/";
+            break;
+        case 14:
+            window.location.href = "/viewer/district_population/";
+            break;
+        case 15:
+            window.location.href = "/viewer/country_population/";
+            break;
+        case 16:
+            window.location.href = "/viewer/continent_population/";
+            break;
+        case 17:
+            window.location.href = "/viewer/city_population/";
+            break; 
+        default:
+            window.location.href = "404";
+            break;
+    }
+
+});
