1fd9e493b1b84a3de6e98a9180fc33cbe453e010
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2022-03-02 02:48:40 +0000

    Moved index.js back to the project root folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express   = require("express");
-const routes    = require("./routes/app");
+const routes    = require("./src/routes/app");
 
 const app   = express();
 const port  = process.env.PORT || 3000;
