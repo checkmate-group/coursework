d19cb9f32cea920462e3d2a6f9b5776e8e1588d6
Nero-DevOps <headt@roehampton.ac.uk>
2022-03-17 19:12:27 +0000

    Update view_table.js

--- src/public/js/view_table.js
+++ src/public/js/view_table.js
@@ -67,6 +67,15 @@ viewTableButton.addEventListener("click", e => {
         case 17:
             window.location.href = "/viewer/city_population/";
             break; 
+        case 18:
+            window.location.href = "/viewer/population_inout_by_continent/";
+            break; 
+        case 19:
+            window.location.href = "/viewer/population_inout_by_country/";
+            break;
+        case 20:
+            window.location.href = "/viewer/population_inout_by_region/";
+            break;
         default:
             window.location.href = "404";
             break;
