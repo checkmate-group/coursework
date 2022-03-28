4bbd387b529f5a85cdc0396246e7f807be77018b
archeris32 <adascalului.bogdan@gmail.com>
2022-03-28 12:14:19 +0100

    change

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const path      = require("path");
 const express   = require("express");
 const routes    = require("./src/routes/app");
-
 const app   = express();
 const port  = process.env.PORT || 3000;
+//part two
+const http = require('http');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const users = require('./src/views/data.js').userDB;
+const server = http.createServer(app);
 
 app.use(express.static("./src/public"));
 
@@ -18,9 +23,60 @@ app.use("/", routes);
 app.use((req, res, next) => {
     res.status(404).render("404");
 });
+app.post('/register', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (!foundUser) {
+    
+            let hashPassword = await bcrypt.hash(req.body.password, 10);
+    
+            let newUser = {
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            };
+            users.push(newUser);
+            console.log('User list', users);
+    
+            res.send("<div align ='center'><h2>Registration successful</h2></div>");
+        } else {
+            res.send("<div align ='center'><h2>Email already used</h2>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (foundUser) {
+    
+            let submittedPass = req.body.password; 
+            let storedPass = foundUser.password; 
+    
+            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+            if (passwordMatch) {
+                let usrname = foundUser.username;
+                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+            } else {
+                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+            }
+        }
+        else {
+    
+            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+            await bcrypt.compare(req.body.password, fakePass);
+    
+            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+
 
 app.listen(port, () => {
     console.log("server runing at http://localhost:" + port);
 })
-
-
---
+++ src/views/data.js
@@ -0,0 +1,4 @@
+
+const userDB = [];
+  
+module.exports = { userDB };
