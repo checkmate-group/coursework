5b6e2852beb5ed57ac65d867b05a545e0db3be32
Nero-DevOps <headt@roehampton.ac.uk>
2022-04-20 16:19:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const path      = require("path");
-const express   = require("express");
-const routes    = require("./src/routes/app");
+const path = require("path");
+const express = require("express");
+const routes = require("./src/routes/app");
+const session = require("express-session");
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app   = express();
-const port  = process.env.PORT || 3000;
-
+app.use(session({ secret: "Shh, its a secret!" }));
 app.use(express.static("./src/public"));
 
 app.set("view engine", "pug");
@@ -16,10 +17,9 @@ app.locals.basedir = path.join(__dirname, "src/views");
 app.use("/", routes);
 
 app.use((req, res, next) => {
-    res.status(404).render("404");
+  res.status(404).render("404");
 });
 
 app.listen(port, () => {
-    console.log("Server running on http://localhost:" + port);
-})
-
+  console.log("Server running on http://localhost:" + port);
+});
