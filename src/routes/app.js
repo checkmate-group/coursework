fa346cb03773777640187198c9cf0179b40bce43
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2022-04-27 14:55:57 +0100

    Slight changes.

--- src/routes/app.js
+++ src/routes/app.js
@@ -10,7 +10,7 @@ const router = express.Router();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const pool = mysql.createPool({
-  host: "172.23.0.1",
+  host: "172.18.0.2",
   port: "3306",
   user: "root",
   password: "password",
